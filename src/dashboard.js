51fbff354ca026e5e3703274e6e0928b2e111b9f
Yago Pajariño <yago977@hotmail.com>
2022-01-12 23:20:43 +0100

    adds dashboard styles and tasks

--- src/dashboard.js
+++ src/dashboard.js
@@ -10,7 +10,9 @@ export default function dashboard(){
     h2q.textContent = "Quick Access"
     let inbox = document.createElement("span")
     inbox.textContent = "Inbox"
+    inbox.classList.toggle("qa")
     let today = document.createElement("span")
+    today.classList.toggle("qa")
     today.textContent = "Today"
 
     quickAccess.appendChild(h2q)
@@ -20,9 +22,19 @@ export default function dashboard(){
 
     let projectsDiv = document.createElement("div")
     projectsDiv.classList.toggle("projectsDiv")
+    let projectsHeaderDiv = document.createElement("div")
+    projectsHeaderDiv.classList.toggle("projHeaders")
+
     let h2p = document.createElement("h2")
+    let addProject = document.createElement("span")
+    addProject.textContent = "New"
+    addProject.classList.toggle("qa")
+    addProject.classList.toggle("grey")
     h2p.textContent = "Projects"
-    projectsDiv.appendChild(h2p)
+    projectsHeaderDiv.appendChild(h2p)
+    projectsHeaderDiv.appendChild(addProject)
+
+    projectsDiv.appendChild(projectsHeaderDiv)
     barraLateral.appendChild(projectsDiv)
 
     let dash = document.createElement("div")
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css'
 import dashboard from "./dashboard.js"
 import icon from "./task.png"
+import task from "./task.js"
 
 let body = document.querySelector("body")
 body.appendChild(dashboard())
+task()
